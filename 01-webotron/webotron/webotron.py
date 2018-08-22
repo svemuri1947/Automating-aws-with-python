966a224424ffe0a768d5c34e164c093cad0e0ddd
svemuri1947 <svemuri1947@gmail.com>
2018-08-21 21:37:07 -0400

    updated the script

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -24,6 +24,52 @@ def list_bucket_objects(bucket):
     for obj in s3.Bucket(bucket).objects.all():
         print (obj)
 
+@cli.command('setup-bucket')
+@click.argument('bucket')
+
+def setup_bucket():
+    "Create and configure bucket"
+    s3_bucket = None
+
+    try:
+        s3_bucket = s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': session.region_name})
+
+    except clientError as e:
+        if e.response['Error']['code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket)
+        else:
+            raise e
+
+        policy = """
+        {
+        "Version":"2012-10-17",
+        "Statement":[{
+        "Sid":"PublicReadForGetBucketObjects",
+        "Effect":"Allow",
+        "Principal": "*",
+            "Action":["s3:GetObject"],
+            "Resource":["arn:aws:s3:::%s/*"
+            ]
+          }
+         ]
+        }
+        """ % s3_bucket.name
+
+        policy = policy.strip()
+
+        pol = s3_bucket.Policy()
+        pol.put(Policy=policy)
+        ws = new_bucket.Website()
+        ws.put(WebsiteConfiguration={
+              'ErrorDocument': {
+                'Key': 'error.html'
+              },
+              'IndexDocument': {
+                'Suffix': 'index.html'
+              }
+              })
+        return
+
 
 if __name__ =='__main__':
     cli()
